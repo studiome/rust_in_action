4623f2badf737de629dd4d051c031db5fb95ce2a
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-04-29 23:14:28 +0900

    use regex crate

--- ch2/grep-lite/src/main.rs
+++ ch2/grep-lite/src/main.rs
@@ -1,14 +1,18 @@
+use regex::Regex;
+
 fn main() {
-    let search_term = "picture";
+    let re = Regex::new("picture").unwrap();
+
     let quote = "\
     Every face, every shop, bedroom window, public-house, and
     dark square is a picture feverishly turned--in search of what?
     It is the same with books. What do we seek through millions of pages?";
 
-    for (i, line) in quote.lines().enumerate() {
-        if line.contains(search_term) {
-            let line_num = i + 1;
-            println!("{}: {}", line_num, line);
+    for line in quote.lines() {
+        let contains_substring = re.find(line);
+        match contains_substring {
+            Some(_) => println!("{}", line),
+            None => (),
         }
     }
 }
